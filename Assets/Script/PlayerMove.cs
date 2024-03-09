09630adf4c44d4309d4b0fa8971b01fdbae7821f
Mizko Danylo <mizko2000y@gmail.com>
2024-03-09 19:28:32 +0200

    Add player move

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float speed = 3f;
-    public float jumpStrenght = 3f;
-    // Start is called before the first frame update
+    public float speed = 3f; //швидкість руху
+    public float jumpStrenght = 3f; //сила стрибка
+    private Rigidbody2D rb2d; //посилання на фізику об'єкта
+    private bool isJump = false; //чи можу я стрибати
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         transform.Translate(Vector2.right * Time.deltaTime * speed);
+        if(Input.GetMouseButtonDown(0) && isJump)
+        {
+            rb2d.AddForce(Vector2.up * jumpStrenght, ForceMode2D.Impulse);
+            isJump = false;
+        }
         
-        
+    } 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isJump = true;
+        }
     }
+
 }
